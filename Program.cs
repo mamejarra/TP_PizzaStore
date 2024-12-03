e2fe2a3c60ec37cd099fb3d98aa301226361cc93
Jarrabousso <diarra@agiltoo.fr>
2024-12-03 21:01:07 +0000

    Mettre à jour un élément

--- Program.cs
+++ Program.cs
@@ -22,11 +22,23 @@ app.UseSwaggerUI(c =>
 });
 app.MapGet("/", () => "Bonjour Sénégal!");
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+//Créer des éléments
 app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
 {
     await db.Pizzas.AddAsync(pizza);
     await db.SaveChangesAsync();
     return Results.Created($"/pizza/{pizza.Id}", pizza);
 });
+//Obtenir un élément
 app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+//Mettre à jour un élément 
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Nom = updatepizza.Nom;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 app.Run();
